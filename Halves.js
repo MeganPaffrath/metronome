a481ac623ab2f80c4cfd25672e090043dff18ee7
MeganPaffrath <meganpaffrath@gmail.com>
2020-06-29 18:57:01 -0700

    buttons render from passing down bpm

--- Halves.js
+++ Halves.js
@@ -10,12 +10,48 @@ class Halves extends React.Component {
     };
   }
 
+  // Generate buttons by one
+
+
+  // <button type="button" onClick={metronome.bind(props.bpm)} className="btn btn-style">{i} BPM</button>
+
 
   render() {
     console.log("BPM is: " + this.props.bpm);
+
+    // Generate buttons by one
+    let startByOne = this.props.bpm;
+    let buttonsByOne = [];
+    for (let i= (startByOne-3); i <= (startByOne+3); i +=1) {
+      buttonsByOne.push(
+        <div key={"b" + i}>
+         <button type="button" className="btn btn-style">{i} BPM</button>
+        </div>
+      );
+    }
+
+    // generate buttons by 5
+    let startByFive = this.props.bpm;
+    let buttonsByFive = [];
+    for (let i= (startByFive-15); i <= (startByFive+15); i +=5) {
+      buttonsByFive.push(
+        <div key={"b" + i}>
+         <button type="button" className="btn btn-style">{i} BPM</button>
+        </div>
+      );
+    }
+
     return [
-      <div key="d1" className="box-left">Hello</div>,
-      <div key="d2" className="box-right">Hello</div>
+      <div key="d1" className="box-left">
+        <div className="box-content">
+          {buttonsByFive}
+        </div>
+      </div>,
+      <div key="d2" className="box-right">
+        <div className="box-content">
+          {buttonsByOne}
+        </div>
+      </div>
     ];
 
   }
